0ee65695b0237c6b7b434daa2638d8329d3008fd
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-08-27 23:19:14 +0600

    basic window

---
+++ .history/tailwind.config_20250827231328.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export default tailwindConfig = {
+
+}
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var()"
+      },
+    },
+  },
+  plugins: [
+    daisyui
+  ],
+};
---
+++ .history/tailwind.config_20250827231421.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export default tailwindConfig = {
+
+}
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)"
+      },
+    },
+  },
+  plugins: [
+    daisyui
+  ],
+};
